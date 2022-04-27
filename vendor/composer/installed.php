5250983491cf629cfd38ee98f5ccaa01d7305f58
jesseythach <94651491+jesseythach@users.noreply.github.com>
2022-04-27 10:40:14 -0400

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '141d51ef0e72e9eea8c89d019080b1d1c164847b',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '141d51ef0e72e9eea8c89d019080b1d1c164847b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
